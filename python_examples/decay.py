4e2bdfaf96af38aee7aacd8d5d68726294f6d074
Matt Bellis <matthew.bellis@gmail.com>
2014-05-19 14:52:28 -0400

    Changed it so that it will plot multiple plots on same figure.

--- python_examples/decay.py
+++ python_examples/decay.py
@@ -5,25 +5,25 @@ Created on Mon May 19 14:03:26 2014
 @author: Amanda
 """
 import pylab
+
+# Bellis changes
+fig = pylab.figure()
+
 x1= [0,1,2,3,4,5,6,7,8]
 y1 = [100,46,20,11,7,2,1,1,0]
 pylab.plot(x1,y1, 'ro')
-pylab.show()
 
 x2 = [0,1,2,3,4,5,6,7,8]
 y2 = [100,48,25,13,7,2,1,1,0]
 pylab.plot(x2,y2,'bo')
-pylab.show()
 
 x3 = [0,1,2,3,4,5,6,7]
 y3 = [100,51,27,14,7,2,1,0]
 pylab.plot(x3,y3,'ko')
-pylab.show()
 
 x4 = [0,1,2,3,4,5,6,7,8]
 y4 = [100,52,30,10,5,2,1,1,0]
 pylab.plot(x3,y3,'go')
-pylab.show()
 
 x5 = [0,1,2,3,4,5,6,7,8]
 y5 = [100,59,28,12,4,1,1,1,0]
